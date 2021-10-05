e140b825a3c06905a097e99b4c8b870e276a74cb
jonathanko500 <jkko9010@gmail.com>
2021-10-04 20:56:19 -0700

    user input check loop
    made while loop checking user input

--- Assignment 2.asm
+++ Assignment 2.asm
@@ -1,12 +1,14 @@
 	.data
 prompt:		.asciiz "Enter a number less than or equal to 100 to convert to binary: \n"		#prompt
-attempt:	.asciiz "Try again: \n"		#prompt
+attempt:	.asciiz "Try again: \n"		#test prompt
+convert:	.asciiz "Converting... \n"		#convert
+bin:		.asciiz " in binary is: \n"	#user numb to bin
 buff:	.byte	32	#32 byte for string
 	.text
 
 main:
 
-	#print string prompt 
+	#print prompt 
 	li	$v0,	4	#loading address of string
 	la	$a0,	prompt	#call prompt 1
 	syscall
@@ -19,22 +21,29 @@ main:
 	addi	$t1,	$t1,	100	#num check
 	
 	
-	#loop
+	#loop to num check
 	ble	$t0,	$t1,	end	#start loop
+	#user input <= 100	pass = out of loop
 	
 loop:	#print string prompt 
 	li	$v0,	4	#loading address of string
-	la	$a0,	attempt	#call prompt 1
+	la	$a0,	attempt	#call attempt
 	syscall
 	
 	#read int
 	li 	$v0,	5	#read int
 	syscall
 	
-	add	$t0,	$t0,	$0
+	add	$t0,	$v0,	$0
 		
 	
 end:	bgt	$t0,	$t1,	loop	#end loop
+	#user input > 100	pass = back into loop
+	
+	#print convert 
+	li	$v0,	4	#loading address of string
+	la	$a0,	convert	#call prompt 1
+	syscall
 	
 	
 	#return
