7c3a794b6485c0f52ff3a5c468448f28c0ce43d2
jonathanko500 <jkko9010@gmail.com>
2021-10-29 20:21:49 -0700

    Assignment 3
    make strlen

--- Assignment 3.asm
+++ Assignment 3.asm
@@ -1,90 +1,64 @@
-.text
 .data
-strMain: .space 100
-blankSpace: .ascii  " "
-endline: .asciiz "\n"
-.globl main
+str:	.word 	32	#string input
+loc:	.word	32	#array of substring
+.text
 
-main:
+
+main:   
+	#read string input
+	li $v0, 8
+	la $a0, str
+    	li $a1, 32
+    	syscall
     
-
-    li $v0, 8
-    la $a0, strMain
-    li $a1, 99
-    syscall
-
-
-    la $a0,strMain
-    jal findLengthString
-    move $a2, $v0
-
-    la $a0, blankSpace
-    jal findLengthString
-    move $a3, $v0 # M
-    sub $a2, $a2, $a3 # N-M
+    	#print string input
+    	li	$v0,	4	
+    	la	$a0,	str	
+    	syscall
     
-
-    la $a0, strMain
-    la $a1, blankSpace
-
-    jal subStringMatch
-    move $t1, $v0
-
-
-    li $v0, 1
-    move $a0, $t1
+    	#call delim function/method
+    	la	$a0, str
+   	#jal delim
+   	jal strlen
+   	
+   	
+   	
+    
+    #return
+    li	$v0,	10
     syscall
     
-exit:
-    li $v0, 10
-    syscall
+ 
+#delim function to get substrings
+#delim:
+	
 
-    lb $t9, endline
 
-findLengthString:
-    li $t0, -1
-    move $s0, $a0
+    	#jr	$ra
+    
+    
 
-    loop_fls:
-        lb $t1, 0($s0)
-        beq $t1, $t9, foundLength
+#find length of string   
+strlen:
+	li $t0, -1	#pointer
+    	move $s0, $a0	#string
+    	
+    	#loop through string and start counting
+    	loop:
+    		lb $t1, 0($s0)	
+    		beq $t1, $t6,maxLen
+    	
+    		#increment counter / pointer
+    		addi $t0, $t0, 1
+        	addi $s0, $s0, 1
+	
+		j loop
+	maxLen:
+		move $v0, $t0
+        	jr $ra
+        	
 
-        addi $t0, $t0, 1
-        addi $s0, $s0, 1
-        j loop_fls
+	
+	
 
-    foundLength:
-        move $v0, $t0
-        jr $ra
-
-subStringMatch:
-    li $t0, 0 #i
-    loop1:
-        bgt $t0,$a2, loop1done  
-        li $t1, 0 #j
-        loop2:
-            bge $t1, $a3, loop2done
-            add $t3, $t0, $t1
-            add $t4, $a0, $t3
-            lb $t3, 0($t4) # main[i+j] 
-
-            add $t4, $a1, $t1
-            lb $t4, 0($t4) # sub[j]
-            # if a0[i + j] != a1[j]
-            bne $t3, $t4, break1
-
-            addi $t1, $t1, 1
-            j loop2
-        loop2done:
-            beq $t1, $a3, yesReturn
-            j break1
-        yesReturn:
-            move $v0, $t0
-            jr $ra
-    break1:
-        addi $t0, $t0, 1
-        j loop1
-    loop1done:
-        li $v0, -1
-        jr $ra
 
