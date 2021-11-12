54764505bfeaf6ab7243bd9af4f08837413195ff
jonathanko500 <jkko9010@gmail.com>
2021-11-11 19:21:51 -0800

    redo stuff

--- MATH324-HW1/HW1.cpp
+++ MATH324-HW1/HW1.cpp
@@ -7,8 +7,7 @@ using namespace std;
 const int rnSize = 40000;
 const int avgSize = 2000;
 
-void fillMeans(double x[] , double y[]);
-
+int[] makeNumbs(int x[]);
 int main()
 {
 	// Get the system time.
@@ -17,41 +16,27 @@ int main()
 	// Seed the random number generator.
 	srand(seed);
 
-	double rnList[rnSize];//array of random numbers between -6 and 10
+	int rnList[5];//array of random numbers between -6 and 10
 	double means[avgSize];//means of nums
 
-
-	fillMeans(rnList, means);
+	rnList = makeNumbs(rnList);
 
 	return 0;
 }
 
 
-//get 2000 mean samples
-void fillMeans(double x[], double y[])
-{//start
-	int sum = 0;
-	double avg;
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{//fill rnList
-			x[j] = (rand() % 16) - 6;
-			sum += x[j];
-			cout << x[j] << endl;
-		}
-		cout << sum << endl;
-		cout << "***********" << endl;
-		avg = sum / 5;
-		y[i] = avg;
-		cout.precision(3);
-		cout << y[i] << endl;
+
+//make array full of random numbers between -6 and 10
+int[] makeNumbs(int x[])
+{
+	int len = sizeof(x) / sizeof(x[0]);
+	for (int j = 0; j < len; j++)
+	{//fill rnList
+		x[j] = (rand() % 16) - 6;
 	}
-	
 
-}//end
-
-
+	return x[];
+}
 
 
 
