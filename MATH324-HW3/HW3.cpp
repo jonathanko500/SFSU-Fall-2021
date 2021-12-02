a92fc51fb8eabecdc86021c861a35b8bb31bec49
jonathanko500 <jkko9010@gmail.com>
2021-12-01 16:47:56 -0800

    Math 324 HW 3
    start

---
+++ MATH324-HW3/HW3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib> 
+#include <ctime> 
+
+using namespace std;
+
+const int sim = 10;//make 10000
+
+int main()
+{
+	//set up randomizer to be random every time
+	srand((unsigned)time(0));
+
+
+	return 0;
+}
